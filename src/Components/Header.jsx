3e9f4743056d814b1a1d7ff4c4ab053174713ee8
João Vitor P.R. <jvprocha.05gmail.com>
2025-09-19 23:13:15 -0300

    Adicionando componentes header e estilizando

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,26 @@
 
 const Header = () => {
     return (
-        <header>
-            <p>asdwasd</p>
+        <header className='bg-orange-700 flex'>
+           <div className='ml-20 p-8 space-x-10 text-white flex text-lg'>
+             <a href="">
+                Logo
+            </a>
+             <a href="" className="hover:underline ">
+                Menu
+             </a>
+             <a href="" className="hover:underline">
+                Combos
+             </a>
+           </div>
+           <div className="space-x-5 mt-0.5 ml-220 flex items-center text-lg">
+            <button className="text-white hover:underline cursor-pointer">
+                Criar conta
+            </button>
+            <button className="bg-white p-2 rounded-full w-20 cursor-pointer transition transform translate-y-0 hover:translate-y-1 active:translate-y-2 ">
+                Entrar
+            </button>
+           </div>
         </header>
     )
 }
