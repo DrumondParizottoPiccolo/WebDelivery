b965781fe48a05737ec8d6820d60a0aeb498f66d
João Vitor P.R. <jvprocha.05gmail.com>
2025-09-20 13:28:47 -0300

    Refazendo estilização header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,23 +3,25 @@ const Header = () => {
     return (
         <header className='bg-orange-700 flex'>
            <div className='ml-20 p-8 space-x-10 text-white flex text-lg'>
-             <a href="">
-                Logo
-            </a>
-             <a href="" className="hover:underline ">
-                Menu
-             </a>
-             <a href="" className="hover:underline">
-                Combos
-             </a>
-           </div>
-           <div className="space-x-5 mt-0.5 ml-220 flex items-center text-lg">
-            <button className="text-white hover:underline cursor-pointer">
-                Criar conta
-            </button>
-            <button className="bg-white p-2 rounded-full w-20 cursor-pointer transition transform translate-y-0 hover:translate-y-1 active:translate-y-2 ">
-                Entrar
-            </button>
+             <nav className=' space-x-10 flex mt-2'>
+                 <a href="">
+                    Logo
+                 </a>
+                 <a href="" className="hover:underline">
+                    Menu
+                 </a>
+                 <a href="" className="hover:underline">
+                    Combos
+                 </a>
+             </nav>
+             <nav className="flex space-x-3 ml-220">
+                <button className="flex mt-2 hover:underline cursor-pointer ">
+                    Criar Conta
+                </button>
+                <button className="bg-white p-2 w-20 rounded-full text-black transform translate-y-0 hover:translate-y-1 cursor-pointer active:translate-y-2">
+                    Entrar
+                </button>
+             </nav>
            </div>
         </header>
     )
