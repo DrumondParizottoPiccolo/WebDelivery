cf38e57dbb5962cc974d17df92a58219379f022d
Paulo Vieira <paulohenriqueavieira@gmail.com>
2025-09-21 12:24:20 -0300

    Merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from './Components/Header.jsx'
 import Section4 from './Components/Section4.jsx'
-
+import Section from './Components/Section.jsx'
 function App() {
 
   return (
     <>
-      {/* <Header/> */}
+      <Header/>
+      <Section/>
       <Section4/>
     </>
   )
