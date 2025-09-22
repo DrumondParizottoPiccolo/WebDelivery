097bab1a626049f6a57bbf2c02754ec9702a2f7d
João Vitor P.R. <jvprocha.05gmail.com>
2025-09-22 10:05:03 -0300

    ;colocando imagem logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,25 @@
 
+
 const Header = () => {
     return (
         <header className='bg-[#45835F] flex'>
            <div className='ml-20 p-8 space-x-10 text-white flex text-lg'>
              <nav className=' space-x-10 flex mt-2'>
                  <a href="">
-                    Logo
+                    <img 
+                    src="/FeedBetter.png" 
+                    alt="Logo food better"
+                    className="w-20" />
                  </a>
-                 <a href="" className="hover:underline">
+                 <a href="" className="mt-5 hover:underline">
                     Menu
                  </a>
-                 <a href="" className="hover:underline">
+                 <a href="" className="mt-5 hover:underline">
                     Combos
                  </a>
              </nav>
-             <nav className="flex space-x-3 ml-220">
-                <button className="flex mt-2 hover:underline cursor-pointer ">
+             <nav className="flex space-x-3  ml-220">
+                <button className="flex mt-2  hover:underline cursor-pointer ">
                     Criar Conta
                 </button>
                 <button className="bg-white p-2 w-20 rounded-full text-black transform translate-y-0 hover:translate-y-1 cursor-pointer active:translate-y-2">
