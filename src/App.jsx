41bf25daf1d8b4637884726f999fa9e791956b42
João Vitor P.R. <jvprocha.05gmail.com>
2025-09-22 16:25:20 -0300

    Adicionando imagem de comida section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Header from './Components/Header.jsx'
 import Section4 from './Components/Section4.jsx'
-import Section from './Components/Section.jsx'
 import Footer from './Components/Footer.jsx'
 import Pratos from './Components/Pratos.jsx'
+import Section1 from './Components/Section1.jsx'
 
 function App() {
 
   return (
     <>
       <Header/>
-      <Section/>
+      <Section1/>
       <Section4/>
       <Pratos/>
       <Footer/>
---
+++ src/Components/Section1.jsx
@@ -0,0 +1,14 @@
+import comum2 from '../assets/icons/comum2.png';
+
+const Section1 = () => {
+    return(
+      <div className="flex w-full h-120 bg-cover bg-center"
+      style={{backgroundImage: `url(${comum2})`}}>
+        <div>
+            
+        </div>
+
+      </div>
+    )
+}
+export default Section1
