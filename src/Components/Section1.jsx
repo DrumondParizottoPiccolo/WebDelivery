4005100772c1e0f8c786d3c57d84942414f934ff
João Vitor P.R. <jvprocha.05gmail.com>
2025-09-24 10:50:25 -0300

    Arrumando responsividade Header e começando Section1

--- src/Components/Section1.jsx
+++ src/Components/Section1.jsx
@@ -1,19 +1,19 @@
-import comum2 from '../assets/section_1_food.png';
+import comum2 from '../assets/section2_1_food.png';
 
 const Section1 = () => {
     return(
       <div className="flex flex-col w-full h-130 bg-cover bg-center bg-no-repeat"
       style={{backgroundImage: `url(${comum2})`}}>
-        <div className="flex flex-col mt-20 ml-220">
-            <h1 className="flex flex-col font-bold text-3xl">
+        <div className="flex flex-col mt-30 ml-2">
+           <div className="ml-2">
+             <h1 className="flex flex-col font-bold text-3xl">
                 Peça em minutos com o Feed Better
             </h1>
-            <p className="flex flex-col mt-2 text-lg font-bold space-y-2">
+            <p className="flex flex-col mt-2 w-130 text-lg font-bold space-y-2">
                 Pratos exclusivos preparados para você aproveitar o melhor da gastronomia gourmet sem sair de casa.
-            </p>
-            <p className="flex flex-col mt-1 text-lg font-bold space-y-2">
                 Basta escolher no app, pedir e saborear-de forma prática, rápida e sofisticada, como você merece.
             </p>
+           </div>
             <div>
                 <button className="flex flex-col text-lg text-white
                  font-bold mr-200 mt-3 bg-black p-2 w-40 rounded-full 
@@ -25,4 +25,4 @@ const Section1 = () => {
       </div>
     )
 }
-export default Section1
+export default Section1;
