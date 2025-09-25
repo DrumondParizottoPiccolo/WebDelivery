cd3954e972b1e5753eac511c7bff63431dced47a
phsushi <paulohenriqueavieira@gmail.com>
2025-09-25 11:03:19 -0300

    Merge: real

--- src/combos/ComboRoute.jsx
+++ src/combos/ComboRoute.jsx
@@ -6,14 +6,9 @@ import ItemCompra from '../Components/ItemCompra';
 
 const ComboRoute = () => {
   return (
-<<<<<<< HEAD
     <div className='w-auto h-150 flex flex-col justify-left'>
       <Filtro/>
       <ItemCompra/>
-=======
-    <div className='w-auto h-150 flex justify-left p-4'>
-      
->>>>>>> a7b16173b3b983db44bfdf0e7bfdb7174259c835
     </div> 
   );
 }
