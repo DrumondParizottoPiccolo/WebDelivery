6696a4e2ccf4d03734a1deb1f4db41001e2929c4
joaovitorpiccolo <joaovitorpiccolo015@gmail.com>
2025-09-24 23:42:37 -0300

    fix

---
+++ src/combos/ComboRoute.jsx
@@ -0,0 +1,14 @@
+import Filtro from '../Components/Filtro';
+import Container from '@mui/material/Container';
+import Carrossel from '../Components/Carrossel';
+
+const ComboRoute = () => {
+  return (
+    <div className='w-auto h-150 flex justify-left p-4'>
+      
+
+    </div> 
+  );
+}
+
+export default ComboRoute;
